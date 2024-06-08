22b790dd41bc7afccf6a063e90153bd88df9dc33
JustinMaina <justinmwangi58@gmail.com>
2024-06-08 22:02:58 +0300

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        matches = []
+        sorted_word = sorted(self.word)
+        for anagram in possible_anagrams:
+            if sorted(anagram) == sorted_word:
+                matches.append(anagram)
+        return matches        
